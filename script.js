328fc1ff526b4efe6a9f8c2a34064eb3a0ed268f
theBryceiverse <65830606+theBryceiverse@users.noreply.github.com>
2020-10-11 10:33:12 -0700

    initial upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.querySelector("h2").textContent = "<!-- Developer -->";
+//https://stackoverflow.com/questions/2651867/showing-html-comment-strings-in-html-files
+//Allows for the "<!-- -->" to be visable
